63849705efa3e6eea64852ed4d03e0f339bb0be9
Jack Cherng <jfcherng@gmail.com>
2017-07-14 17:44:29 +0800

    Revise tf_helloworld.py

--- jfcherng/tf_helloworld.py
+++ jfcherng/tf_helloworld.py
@@ -3,7 +3,6 @@ import tensorflow as tf
 with tf.Session() as sess:
     a = tf.constant([1.0, 2.0], name="a")
     b = tf.constant([2.0, 3.0], name="b")
-    c = a + b
+    c = tf.add(a, b, name="c")
 
-    result = sess.run(c)
-    print(result)
+    print(sess.run(c))
