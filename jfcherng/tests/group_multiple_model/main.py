e938c9cf463e96aa9c1bfe13d81c7e5ec7a6cd9d
Jack Cherng <jfcherng@gmail.com>
2017-07-19 10:15:37 +0800

    Change tests file/script

--- jfcherng/tests/group_multiple_model/main.py
+++ jfcherng/tests/group_multiple_model/main.py
@@ -1,15 +1,16 @@
 import tensorflow as tf
 
-with tf.Session() as sess:
+with tf.name_scope('Model_A'):
     a = tf.constant([1.0, 2.0], name="a")
     b = tf.constant([2.0, 3.0], name="b")
-    c = tf.add(a, b, name="c")
+    c = tf.add(a, b, name="c.add")
 
-with tf.Session() as sess:
+with tf.name_scope('Model_B'):
     p = tf.constant(8, name="p")
     q = tf.constant(3, name="q")
-    r = tf.multiply(p, q, name="r")
+    r = tf.multiply(p, q, name="r.mul")
+
+M = tf.group(c, r, name='M.grp')
 
 with tf.Session() as sess:
-    M = tf.group(c, r, name='M')
     sess.run(M)
