512c581e4289a1dc7cd67ada85ccceb4d833db10
Jack Cherng <jfcherng@gmail.com>
2017-07-19 10:15:37 +0800

    Change tests file/script

--- jfcherng/tests/run_helloworld.sh
+++ jfcherng/tests/run_helloworld.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 # configs
+GraphOutputDir=/tmp/tensorflow
+
 JFCHERNG_DEBUG=1
 TF_CPP_MIN_VLOG_LEVEL=0 # to dump XLA graph, this should be >= 1
-TF_XLA_FLAGS="--tf_dump_graph_prefix=/tmp/tensorflow --xla_generate_hlo_graph=.*"
+TF_XLA_FLAGS="--tf_dump_graph_prefix=${GraphOutputDir} --xla_hlo_dump_graph_path=${GraphOutputDir} --xla_generate_hlo_graph=.*"
 
+# execute
 TF_XLA_FLAGS="${TF_XLA_FLAGS}" \
 JFCHERNG_DEBUG="${JFCHERNG_DEBUG}" \
 TF_CPP_MIN_VLOG_LEVEL="${TF_CPP_MIN_VLOG_LEVEL}" \
