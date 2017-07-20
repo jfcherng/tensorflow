59688b4b510db6747d1bd61e5b20a7a799219d10
Jack Cherng <jfcherng@gmail.com>
2017-07-20 09:16:24 +0800

    nits

--- jfcherng/tests/run_tf_script.sh
+++ jfcherng/tests/run_tf_script.sh
@@ -7,6 +7,9 @@ JFCHERNG_DEBUG=1
 TF_CPP_MIN_VLOG_LEVEL=1 # to dump XLA graph, this should be >= 1
 TF_XLA_FLAGS="--tf_dump_graph_prefix=${GraphOutputDir} --xla_hlo_dump_graph_path=${GraphOutputDir} --xla_generate_hlo_graph=.*"
 
+# prepare
+mkdir -p ${GraphOutputDir}
+
 # execute
 TF_XLA_FLAGS="${TF_XLA_FLAGS}" \
 JFCHERNG_DEBUG="${JFCHERNG_DEBUG}" \
