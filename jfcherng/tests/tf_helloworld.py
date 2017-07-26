571b64eb222303312536f42bec66f5e2e2b7a14c
Jack Cherng <jfcherng@gmail.com>
2017-07-26 14:43:58 +0800

    [tf_helloworld.py] Avoid constant folding

--- jfcherng/tests/tf_helloworld.py
+++ jfcherng/tests/tf_helloworld.py
@@ -7,10 +7,16 @@ config = tf.ConfigProto(log_device_placement=True)
 # config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
 
 with tf.Session(config=config) as sess:
-    a = tf.constant([1.0, 2.0], name="a")
-    b = tf.constant([2.0, 3.0], name="b")
+    a = tf.placeholder(tf.float32, [2], name="a")
+    b = tf.placeholder(tf.float32, [2], name="b")
 
     with tf.device("/job:localhost/replica:0/task:0/device:XLA_CPU:0"):
         c = tf.add(a, b, name="c")
 
-    print(sess.run(c))
+    print(sess.run(
+        c,
+        {
+            a: [1.0, 3.0],
+            b: [2.0, 5.0],
+        }
+    ))
