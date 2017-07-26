11a13696bc779c64cc14289ffabb7a755e335976
Jack Cherng <jfcherng@gmail.com>
2017-07-26 18:47:55 +0800

    Add relu test

--- jfcherng/tests/tf_helloworld.py
+++ jfcherng/tests/tf_helloworld.py
@@ -7,16 +7,31 @@ config = tf.ConfigProto(log_device_placement=True)
 # config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
 
 with tf.Session(config=config) as sess:
-    a = tf.placeholder(tf.float32, [2], name="a")
-    b = tf.placeholder(tf.float32, [2], name="b")
 
+    a = tf.constant([[1.0, -3.0]], name="a")
+    b = tf.Variable([[2.0, 5.0, 7.0], [2.0, 5.0, 7.0], ], name="b")
+    e = tf.placeholder(tf.float32, [1, 3], name="e")
+
+    sess.run(tf.global_variables_initializer())
     with tf.device("/job:localhost/replica:0/task:0/device:XLA_CPU:0"):
-        c = tf.add(a, b, name="c")
+        c = tf.matmul(a, b, name="c")
+        result = tf.concat([c, e], 0, name='result')
+
+
+        reluTest= tf.nn.relu(e, name='reluTest')
 
     print(sess.run(
-        c,
+        reluTest,
         {
-            a: [1.0, 3.0],
-            b: [2.0, 5.0],
+            # a: [
+            #     [1.0, 3.0]
+            # ],
+            # b: [
+            #     [2.0, 5.0, 7.0],
+            #     [2.0, 5.0, 7.0],
+            # ],
+            e: [
+                [4.0, -5.0, 6.0],
+            ],
         }
     ))
