2195358e3c9ea9fa0ea28845a5bf6d435db132b9
Jack Cherng <jfcherng@gmail.com>
2017-07-26 18:48:09 +0800

    Add some comments to run_tf_script.sh

--- jfcherng/tests/run_tf_script.sh
+++ jfcherng/tests/run_tf_script.sh
@@ -9,13 +9,24 @@ GraphOutputDir=/tmp/tensorflow
 JFCHERNG_DEBUG=1
 TF_CPP_MIN_VLOG_LEVEL=2 # to dump XLA graph, this should be >= 1
 TF_XLA_FLAGS_ARRAY=(
+    # terms:
+    #     log  = show message on the screen
+    #     dump = write to an external file
+
+    # output directory
     "--tf_dump_graph_prefix=${GraphOutputDir}"
-    "--xla_hlo_dump_graph_path=${GraphOutputDir}"
     "--xla_dump_computations_to=${GraphOutputDir}"
+    "--xla_hlo_dump_graph_path=${GraphOutputDir}"
+
+    # this flag will dump HLO graph (disregard TF_CPP_MIN_VLOG_LEVEL)
     "--xla_generate_hlo_graph=.*"
+
+    # the format of dumped HLO IR
+    # true / false = pbtxt / dot
+    "--xla_hlo_dump_as_graphdef=true"
+
+    # show HLO IR (text form) on the screen
     "--xla_log_hlo_text=.*"
-    "--xla_hlo_dump_as_graphdef=1"
-    "--xla_hlo_profile=1"
 )
 
 ##############
@@ -25,6 +36,7 @@ TF_XLA_FLAGS_ARRAY=(
 TF_XLA_FLAGS=${TF_XLA_FLAGS_ARRAY[*]}
 
 mkdir -p ${GraphOutputDir}
+rm -f "${GraphOutputDir}"/*
 
 ##############
 # export ENV #
